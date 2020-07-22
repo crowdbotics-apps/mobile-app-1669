31e985cbbad517f941d69e8feabacce7a9298b3f
Crowdbotics <team@crowdbotics.com>
2020-07-22 05:58:58 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,1 +1,8 @@
 import axios from "axios"
+import {} from "react-native-dotenv"
+const newPlugin221 = axios.create({
+  baseURL:
+    "https://screen-editor-review.herokuapp.com/dashboard/app/1669/storyboard/243/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+export const apiService = {}
